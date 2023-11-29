f5b609cd4222a4ce4ffbaaeb96fb144e5a2bd74f
Paresh-d <paresh76d@gmail.com>
2023-11-28 22:01:04 -0500

    first commit

---
+++ billing/billing_payment.java
@@ -0,0 +1,11 @@
+package billing;
+
+public class billing_payment {
+static int a =50;
+static String myName ="Paresh";
+public static void main(String[] args) {
+	System.out.println(a);
+	System.out.println("myname");
+}
+
+}
